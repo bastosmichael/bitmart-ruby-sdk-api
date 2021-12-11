d3de8d7531efe59321b681c943bbe13bb9d5b886
Michael Bastos <bastosmichael@gmail.com>
2021-12-10 23:09:05 -0600

    fixing models

--- lib/bitmart/api_account.rb
+++ lib/bitmart/api_account.rb
@@ -58,7 +58,7 @@ module Bitmart
 
                 # POST https://api-cloud.bitmart.com/account/v1/withdraw/apply
                 def post_withdraw_apply(currency, amount, destination, address, address_memo)
-                    param = {
+                    params = {
                         'currency': currency,
                         'amount': amount,
                         'destination': destination,
@@ -66,7 +66,7 @@ module Bitmart
                         'address_memo': address_memo
                     }
                     request(
-                        http_method: :get,
+                        http_method: :post,
                         endpoint: "withdraw/apply"
                         params: params
                     )
@@ -74,7 +74,7 @@ module Bitmart
 
                 # GET https://api-cloud.bitmart.com/account/v1/deposit-withdraw/history
                 def get_deposit_withdraw_history(currency, operationType, offset, limit)
-                    param = {
+                    params = {
                         'currency': currency,
                         'operation_type': operationType,
                         'offset': offset,
@@ -89,7 +89,7 @@ module Bitmart
 
                 # GET https://api-cloud.bitmart.com/account/v1/deposit-withdraw/detail
                 def get_deposit_withdraw_detail(id)
-                    param = {
+                    params = {
                         'id': id
                     }
                     request(
@@ -118,63 +118,3 @@ module Bitmart
         end
     end
 end
-
-
-
-class APIAccount(CloudClient):
-
-    def __init__(self, api_key, secret_key, memo, url: str = API_URL, timeout: tuple = TIMEOUT):
-        CloudClient.__init__(self, api_key, secret_key, memo, url, timeout)
-
-    # GET https://api-cloud.bitmart.com/account/v1/currencies
-    def get_currencies(self):
-        return self._request_without_params(GET, API_ACCOUNT_CURRENCIES_URL)
-
-    # GET https://api-cloud.bitmart.com/account/v1/wallet
-    def get_wallet(self, accountType: str):
-        param = {
-            'account_type': accountType
-        }
-        return self._request_with_params(GET, API_ACCOUNT_WALLET_URL, param, Auth.KEYED)
-
-    # GET https://api-cloud.bitmart.com/account/v1/deposit/address
-    def get_deposit_address(self, currency: str):
-        param = {
-            'currency': currency
-        }
-        return self._request_with_params(GET, API_ACCOUNT_DEPOSIT_ADDRESS_URL, param, Auth.KEYED)
-
-    # GET https://api-cloud.bitmart.com/account/v1/withdraw/charge
-    def get_withdraw_charge(self, currency: str):
-        param = {
-            'currency': currency
-        }
-        return self._request_with_params(GET, API_ACCOUNT_WITHDRAW_CHARGE_URL, param, Auth.KEYED)
-
-    # POST https://api-cloud.bitmart.com/account/v1/withdraw/apply
-    def post_withdraw_apply(self, currency: str, amount: str, destination: str, address: str, address_memo: str):
-        param = {
-            'currency': currency,
-            'amount': amount,
-            'destination': destination,
-            'address': address,
-            'address_memo': address_memo
-        }
-        return self._request_with_params(POST, API_ACCOUNT_WITHDRAW_APPLY_URL, param, Auth.SIGNED)
-
-    # GET https://api-cloud.bitmart.com/account/v1/deposit-withdraw/history
-    def get_deposit_withdraw_history(self, currency: str, operationType: str, offset: int, limit: int):
-        param = {
-            'currency': currency,
-            'operation_type': operationType,
-            'offset': offset,
-            'limit': limit
-        }
-        return self._request_with_params(GET, API_ACCOUNT_DEPOSIT_WITHDRAW_HISTORY_URL, param, Auth.KEYED)
-
-    # GET https://api-cloud.bitmart.com/account/v1/deposit-withdraw/detail
-    def get_deposit_withdraw_detail(self, id: str):
-        param = {
-            'id': id
-        }
-        return self._request_with_params(GET, API_ACCOUNT_DEPOSIT_WITHDRAW_DETAIL, param, Auth.KEYED)
---
+++ lib/bitmart/api_contract.rb
@@ -0,0 +1,261 @@
+module Bitmart
+    module API
+        module V1
+            class Contract
+                API_ENDPOINT = 'https://api-cloud.bitmart.com/account/v1'.freeze
+
+                attr_reader :api_key
+                attr_reader :api_sign
+
+                def initialize(api_key = nil, api_sign = nil)
+                    @api_key = api_key
+                    @api_sign = api_sign
+                end
+
+                    # GET https://api-cloud.bitmart.com/contract/v1/ifcontract/contracts
+                def get_contracts
+                    params = {'exchange': 'bitmart'}
+                    request(
+                        http_method: :get,
+                        endpoint: "ifcontract/contracts"
+                        params: params
+                    )
+                end
+
+                # GET https://api-cloud.bitmart.com/contract/v1/ifcontract/pnls
+                def get_pnls(contractId)
+                    params = {'contractID': contractId}
+                    request(
+                        http_method: :get,
+                        endpoint: "ifcontract/pnls"
+                        params: params
+                    )
+                end
+
+                # GET https://api-cloud.bitmart.com/contract/v1/ifcontract/indexes
+                def get_indexes
+                    request(
+                        http_method: :get,
+                        endpoint: "ifcontract/indexes"
+                        params: params
+                    )
+                end
+
+                # GET https://api-cloud.bitmart.com/contract/v1/ifcontract/tickers
+                def get_tickers(self, contractId: int):
+                    params = {}
+                    params = {'contractID': contractId} if contractId   
+                    request(
+                        http_method: :get,
+                        endpoint: "ifcontract/tickers"
+                        params: params
+                    )
+                end
+
+                # GET https://api-cloud.bitmart.com/contract/v1/ifcontract/quote
+                def get_quote(self, contractId: int, startTime: int, endTime: int, unit: int, resolution: str):
+                    params = {'contractID': contractId, 'startTime': startTime, 'endTime': endTime, 'unit': unit, 'resolution': resolution}
+                    request(
+                        http_method: :get,
+                        endpoint: "ifcontract/quote"
+                        params: params
+                    )
+                end
+
+                # GET https://api-cloud.bitmart.com/contract/v1/ifcontract/indexquote
+                def get_index_quote(indexId, startTime, endTime, unit, resolution):
+                    params = {'indexID': indexId, 'startTime': startTime, 'endTime': endTime, 'unit': unit, 'resolution': resolution}
+                    request(
+                        http_method: :get,
+                        endpoint: "ifcontract/indexquote"
+                        params: params
+                    )
+                end
+
+                # GET https://api-cloud.bitmart.com/contract/v1/ifcontract/trades
+                def get_trades(contractId)
+                    params = {'contractID': contractId}
+                    request(
+                        http_method: :get,
+                        endpoint: "ifcontract/trades"
+                        params: params
+                    )
+                end
+
+                # GET https://api-cloud.bitmart.com/contract/v1/ifcontract/depth
+                def get_depth(contractId, count)
+                    params = {'contractID': contractId}
+                    if count:
+                        param['count'] = count
+                    request(
+                        http_method: :get,
+                        endpoint: "ifcontract/depth"
+                        params: params
+                    )
+                end
+
+                # GET https://api-cloud.bitmart.com/contract/v1/ifcontract/fundingrate
+                def get_funding_rate(contractId)
+                    params = {'contractID': contractId}
+                    request(
+                        http_method: :get,
+                        endpoint: "ifcontract/fundingrate"
+                        params: params
+                    )
+                end
+
+                # Order API
+                # GET https://api-cloud.bitmart.com/contract/v1/ifcontract/userOrders
+                def get_user_orders(contractId, status, offset = nil, size = nil)
+                    params = {'contractID': contractId, 'status': status}
+                    if offset && size
+                        params['offset'] = offset
+                        params['size'] = size
+                    end
+                    request(
+                        http_method: :get,
+                        endpoint: "ifcontract/userOrders"
+                        params: params
+                    )
+                end
+
+                # GET https://api-cloud.bitmart.com/contract/v1/ifcontract/userOrderInfo
+                def get_user_order_info(contractId, orderId)
+                    params = {'contractID': contractId, 'orderID': orderId}
+                    request(
+                        http_method: :get,
+                        endpoint: "ifcontract/userOrderInfo"
+                        params: params
+                    )
+                end
+
+                # POST https://api-cloud.bitmart.com/contract/v1/ifcontract/submitOrder
+                def post_submit_order(contractId, category, way, openType, leverage, customId, price, vol)
+                    params = {
+                        'contract_id': contractId,
+                        'category': category,
+                        'way': way,
+                        'open_type': openType,
+                        'leverage': leverage,
+                        'custom_id': customId,
+                        'price': price,
+                        'vol': vol
+                    }
+                    request(
+                        http_method: :post,
+                        endpoint: "ifcontract/submitOrder"
+                        params: params
+                    )
+                end
+
+                # POST https://api-cloud.bitmart.com/contract/v1/ifcontract/batchOrders
+                def post_submit_batch_order(self, orders):
+                    params = {'orders': orders}
+                    request(
+                        http_method: :get,
+                        endpoint: "ifcontract/batchOrders"
+                        params: params
+                    )
+                end
+
+                # POST https://api-cloud.bitmart.com/contract/v1/ifcontract/cancelOrders
+                def post_cancel_order(contractId, orders):
+                    params = {'contractID': contractId, 'orders': orders}
+                    request(
+                        http_method: :get,
+                        endpoint: "ifcontract/cancelOrders"
+                        params: params
+                    )
+                end
+
+                # GET https://api-cloud.bitmart.com/contract/v1/ifcontract/userTrades
+                def get_user_trades(self, contractId: int, offset: int, size: int):
+                    params = {'contractID': contractId}
+                        params['offset'] = offset if offset
+                        params['size'] = size if size
+                    request(
+                        http_method: :get,
+                        endpoint: "ifcontract/userTrades"
+                        params: params
+                    )
+                end
+
+                # GET https://api-cloud.bitmart.com/contract/v1/ifcontract/orderTrades
+                def get_order_trades(contractId, orderId)
+                    params = {'contractID': contractId, 'orderID': orderId}
+                    request(
+                        http_method: :get,
+                        endpoint: "ifcontract/orderTrades"
+                        params: params
+                    )
+                end
+
+                # GET https://api-cloud.bitmart.com/contract/v1/ifcontract/accounts
+                def get_accounts(coinCode)
+                    params = {'coinCode': coinCode}
+                    request(
+                        http_method: :get,
+                        endpoint: "ifcontract/userOrders"
+                        params: params
+                    )
+                end
+
+                # GET https://api-cloud.bitmart.com/contract/v1/ifcontract/userPositions
+                def get_user_positions(contractId)
+                    params = {'contractID': contractId}
+                    request(
+                        http_method: :get,
+                        endpoint: "ifcontract/userPositions"
+                        params: params
+                    )
+                end
+
+                # GET https://api-cloud.bitmart.com/contract/v1/ifcontract/userLiqRecords
+                def get_user_liq_records(contractId, orderId)
+                    params = {'contractID': contractId, 'orderID': orderId}
+                    request(
+                        http_method: :get,
+                        endpoint: "ifcontract/userLiqRecords"
+                        params: params
+                    )
+                end
+
+                # GET https://api-cloud.bitmart.com/contract/v1/ifcontract/positionFee
+                def get_position_fee(contractId, positionId)
+                    params = {'contractID': contractId, 'positionID': positionId}
+                    request(
+                        http_method: :get,
+                        endpoint: "ifcontract/positionFee"
+                        params: params
+                    )
+                end
+
+                # POST https://api-cloud.bitmart.com/contract/v1/ifcontract/marginOper
+                def post_margin_oper(contractId, positionId, vol, operType)
+                    params = {'contract_id': contractId, 'position_id': positionId, 'vol': vol, 'oper_type': operType}
+                    request(
+                        http_method: :post,
+                        endpoint: "ifcontract/marginOper"
+                        params: params
+                    )
+                end
+
+                private
+
+                def client
+                    @_client ||= Faraday.new(API_ENDPOINT) do |client|
+                    client.request :url_encoded
+                    client.adapter Faraday.default_adapter
+                    client.headers['X-BM-KEY'] = api_key if api_key.present?
+                    client.headers['X-BM-SIGN'] = api_sign if api_sign.present?
+                    end
+                end
+
+                def request(http_method:, endpoint:, params: {})
+                    response = client.public_send(http_method, endpoint, params)
+                    Oj.load(response.body)
+                end
+            end
+        end
+    end
+end
