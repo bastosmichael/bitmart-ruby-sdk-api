d21ea32d59387e012d0823db33b0a056590df070
Michael Bastos <bastosmichael@gmail.com>
2021-12-11 19:25:53 -0600

    adding required

--- lib/bitmart.rb
+++ lib/bitmart.rb
@@ -1,5 +1,8 @@
 require "bitmart/version"
 require "bitmart/api_account"
+require "bitmart/api_contract"
+require "bitmart/api_spot"
+require "bitmart/api_system"
 
 module Bitmart
   class Error < StandardError; end
