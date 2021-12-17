c785ffbd2bcf718c37936becf4683832f30bb1b8
Michael Bastos <bastosmichael@gmail.com>
2021-12-17 16:35:55 -0600

    updating models

--- lib/bitmart/api_account.rb
+++ lib/bitmart/api_account.rb
@@ -8,7 +8,7 @@ module Bitmart
                 attr_reader :api_sign
                 attr_reader :api_memo
 
-                def initialize(api_key = nil, api_signature = nil, api_memo = nil)
+                def initialize(api_key = nil, api_sign = nil, api_memo = nil)
                     @api_key = api_key
                     @api_sign = api_sign
                     @api_memo = api_memo
@@ -108,10 +108,10 @@ module Bitmart
                     client.request :url_encoded
                     client.adapter Faraday.default_adapter
                     timestamp = Time.now.getutc.to_i.to_s
-                    signature = [timestamp,"#",api_memo,"#",client.params.to_s].join if api_memo&.present?
+                    signature = [timestamp,"#",api_memo,"#",client.params.to_s].join unless api_memo&.nil?
                     signed = OpenSSL::HMAC.hexdigest("SHA256", signature, api_sign) if signature
-                    client.headers['X-BM-KEY'] = api_key if api_key&.present?
-                    client.headers['X-BM-SIGN'] = signed if signed&.present?
+                    client.headers['X-BM-KEY'] = api_key unless api_key&.nil?
+                    client.headers['X-BM-SIGN'] = signed unless signed&.nil?
                     client.headers['X-BM-TIMESTAMP'] = timestamp
                     end
                 end
